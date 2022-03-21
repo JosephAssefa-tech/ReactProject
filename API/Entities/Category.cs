064d3cf6d62651344186dedda4c70b83c06d6708
Joseph <yosephassefa85@gmail.com>
2022-03-21 22:54:38 +0300

    caterogry properties done

--- API/Entities/Category.cs
+++ API/Entities/Category.cs
@@ -7,5 +7,8 @@ namespace API.Entities
 {
     public class Category
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
     }
 }
