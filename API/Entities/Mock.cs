a3f363a8a6d251ba2c145ecf3f7d9fc6e6a76aab
Joseph <yosephassefa85@gmail.com>
2022-03-25 15:09:33 +0300

    adding new model

--- API/Entities/Mock.cs
+++ API/Entities/Mock.cs
@@ -11,8 +11,7 @@ namespace API.Entities
         public string name { get; set; }
         public string itemName { get; set; }
         public Product mock { get; set; }
-        public Category category { get; set; }
-        
+        public Category category { get; set; } 
 
     }
 }
---
+++ API/Entities/Testing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Testing
+    {
+        public Guid guid { get; set; }
+        public string name { get; set; }
+    }
+}
