6835ff1f0ecbb910b059f7b48196580efe871264
Joseph <yosephassefa85@gmail.com>
2022-04-02 22:11:46 +0300

    new properties in category

--- API/Entities/Category.cs
+++ API/Entities/Category.cs
@@ -9,5 +9,8 @@ namespace API.Entities
     {
         public int id { get; set; }
         public string name { get; set; }
+        public string ProductId { get; set; }
+        public DateTime date { get; set; }
+
     }
 }
