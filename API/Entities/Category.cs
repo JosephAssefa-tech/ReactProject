720878c2897a6b7f1c19465308455200e66886f2
Joseph <yosephassefa85@gmail.com>
2022-04-03 14:02:54 +0300

    done entity

--- API/Entities/Category.cs
+++ API/Entities/Category.cs
@@ -13,7 +13,7 @@ namespace API.Entities
         public int id { get; set; }
         public string name { get; set; }
  
-        public DateTime date { get; set; }
+       
         public Product products { get; set; }
         
 
