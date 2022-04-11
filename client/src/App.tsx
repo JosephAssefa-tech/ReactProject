7bf74ed50e8d55b1935003a276bf8f8a39e60429
Joseph <yosephassefa85@gmail.com>
2022-04-11 20:57:22 +0300

    working in client project and create a componenet

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+import React from 'react';
+import logo from './logo.svg';
+
+const product=[
+  {name:'product1',price:100},
+  {name:'product2',price:100}
+]
+
+function App() {
+  return (
+    <div className="App">
+ <h1>{product.name}</h1>
+    </div>
+  );
+}
+
+export default App;
