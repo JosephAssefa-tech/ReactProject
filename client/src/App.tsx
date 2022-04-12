ea9a3ce78239beb11ad7078c7dbdfe5b366ca819
Joseph <yosephassefa85@gmail.com>
2022-04-12 20:43:43 +0300

    adding seed data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,11 @@ import React from 'react';
 import logo from './logo.svg';
 
 const product=[
-  {name:'product111',price:100},
-  {name:'product222',price:100}
+  {name:'product111',price:10},
+  {name:'product222',price:190},
+  {name:'product3',price:100},
+  {name:'product33',price:100},
+  
 ]
 
 function App() {
