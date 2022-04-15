252dfa218f97fcf805ac68377bec5401661fcaf8
Joseph <yosephassefa85@gmail.com>
2022-04-15 17:07:34 +0300

    adding key for list values with indexing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,17 +6,20 @@ import logo from './logo.svg';
 const product=[
   {name:'product111',price:10},
   {name:'product222',price:190},
-  {name:'product3',price:100},
-  {name:'product33',price:100},
+  {name:'product222',price:190},
   
 ]
 
 function App() {
   return (
     <div className="App">
- <h1>{product.map(items=>(
-   <li>{items.name} - {items.price}</li>
- ))}</h1>
+ <h1>{
+ product.map((items,index)=>(
+   <li key={index}>{items.name} - {items.price}</li>
+ ))
+ }
+ 
+ </h1>
     </div>
   );
 }
