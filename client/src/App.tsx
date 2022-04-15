58f8d637c55cd5545644bcf7e2908922b5362e94
Joseph <yosephassefa85@gmail.com>
2022-04-15 20:23:28 +0300

    creating model for accepting data from api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 
 import React, { useEffect } from 'react';
 
+import { Product } from './product';
 import logo from './logo.svg';
 import { useState } from 'react';
 
@@ -13,21 +14,24 @@ const product=[
 ]
 
 function App() {
-  const [product,setProduct]=useState([
-    {name:'product111',price:10},
-    {name:'product222',price:190},
-    {name:'product22',price:190},
-    
-  ]);
+  const [product,setProduct]=useState<Product[]>([]);
   useEffect(()=>{
     fetch('https://localhost:44341/api/Products')
     .then(response=>
       response.json())
     .then(data=>setProduct(data))
   },[])
-  function AddProduct()
-  {
-    setProduct([...product,{name:'product4',price:4}])
+  function AddProduct(){
+    setProduct(prevState => [...prevState,
+      {
+      id:prevState.length + 101,
+      name:'name'+(prevState.length+1),
+      price:(prevState.length*100)+100,
+      brand:'brand',
+      description:'some descritpiion',
+      pictureUrl:'http://picsum.photos/200'
+    
+    }])
   }
 
   return (
