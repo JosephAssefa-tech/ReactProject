40ea4802587b94f0af0825e730f59c4b41369b89
Joseph <yosephassefa85@gmail.com>
2022-04-18 23:24:24 +0300

    removing unused library

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,7 +1,4 @@
-import { Avatar, Button, ListItemAvatar, ListItemText } from "@mui/material";
-
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
 import { Product } from "../../app/layout/models/product";
 import ProductCard from "./ProductCard";
 
