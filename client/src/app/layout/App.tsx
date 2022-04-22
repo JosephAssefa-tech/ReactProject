382da7ee877d31d88b8f735d678283ae872bc231
Joseph <yosephassefa85@gmail.com>
2022-04-22 23:50:44 +0300

    removing unused libraries

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -7,12 +7,9 @@ import Catalog from '../../features/catalog/catalog';
 import ContactPage from '../../features/contact/ContactPage';
 import Header from './Header';
 import HomePage from '../../features/home/HomePage';
-import { Product } from './models/product';
 import ProductDetails from '../../features/catalog/ProductDetails';
 import { Route } from 'react-router-dom';
 import { createTheme } from '@mui/system';
-import { useEffect } from 'react';
-import { useState } from 'react';
 
 function App() {
   const theme=createTheme(
