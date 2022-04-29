6c988810dbcac32dd7be96380370335755443bd4
Joseph <yosephassefa85@gmail.com>
2022-04-29 23:53:41 +0300

    adding toast button

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -8,6 +8,7 @@ export default function AboutPage()
        <Container>
            <Typography gutterBottom variant='h2'>Errors for testing purpose</Typography>
            <ButtonGroup fullWidth>
+           <Button variant='contained' onClick={()=>agent.TestError.getValidationError().catch(error=>console.log(error))}>Toast Notification</Button>
                <Button variant='contained' onClick={()=>agent.TestError.get400Error().catch(error=>console.log(error))}>Test 400 Error</Button>
                <Button variant='contained' onClick={()=>agent.TestError.get401Error().catch(error=>console.log(error))}>Test 401 Error</Button>
                <Button variant='contained' onClick={()=>agent.TestError.get404Error().catch(error=>console.log(error))}>Test 404 Error</Button>
