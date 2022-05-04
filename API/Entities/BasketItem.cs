896f1103723e32273ab3aaa23412aacbbad0d176
Joseph <yosephassefa85@gmail.com>
2022-05-04 17:45:29 +0300

    Adding new Basket Entity

---
+++ API/Entities/BasketItem.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Entities
+{
+    public class BasketItem
+    {
+        public int id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+    }
+}
