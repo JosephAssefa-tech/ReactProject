979c082dc2a073d8fb497538787c0854926dfe14
Joseph <yosephassefa85@gmail.com>
2022-05-10 22:12:09 +0300

    labling server error

--- client/src/app/layout/errors/ServerError.tsx
+++ client/src/app/layout/errors/ServerError.tsx
@@ -4,7 +4,7 @@ export default function ServerError()
 {
     return(
         <Container component={Paper}>
-            <Typography variant='h5' gutterBottom>500 Server Error</Typography>
+            <Typography variant='h5' gutterBottom>Server Error</Typography>
         </Container>
     )
 }
