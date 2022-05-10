999b0ed954e7f81b4ddac15d583f500fdc451311
Joseph <yosephassefa85@gmail.com>
2022-05-10 22:12:39 +0300

    variant value changed

--- client/src/app/layout/errors/ServerError.tsx
+++ client/src/app/layout/errors/ServerError.tsx
@@ -4,7 +4,7 @@ export default function ServerError()
 {
     return(
         <Container component={Paper}>
-            <Typography variant='h5' gutterBottom>Server Error</Typography>
+            <Typography variant='h3' gutterBottom>Server Error</Typography>
         </Container>
     )
 }
