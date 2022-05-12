5607a1c35984a7d3b2d8cb5c1e240e16d6e49843
Joseph <yosephassefa85@gmail.com>
2022-05-12 15:17:59 +0300

    adding entity framwork core relation among entities

--- API/Data/Migrations/20220512121232_BaseEntityAdded.Designer.cs
+++ API/Data/Migrations/20220512121232_BaseEntityAdded.Designer.cs
@@ -3,14 +3,16 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace API.Migrations
+namespace API.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20220512121232_BaseEntityAdded")]
+    partial class BaseEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- API/Entities/BasketItem.cs
+++ API/Entities/BasketItem.cs
@@ -1,13 +1,20 @@
-﻿namespace API.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
 {
+    [Table("BasketItems")]
     public class BasketItem
-    {
+    { 
         public int id { get; set; }
 
         public int Quantity { get; set; }
 
         public int ProductId { get; set; }
         public Product Product { get; set; }
+
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
+         
 
     }
 }
